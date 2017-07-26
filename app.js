7d9ddfbbb1e1ae513e06b8a1a4e6dc629d1db2a3
lyneca <lukemtuthill@gmail.com>
2017-07-26 14:15:00 +1000

    fixed app.js

--- app.js
+++ app.js
@@ -395,6 +395,20 @@ function sendImageMessage(recipientId) {
   callSendAPI(messageData);
 }
 
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText,
+      metadata: "DEVELOPER_DEFINED_METADATA"
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
 /*
  * Send a message with Quick Reply buttons.
  *
